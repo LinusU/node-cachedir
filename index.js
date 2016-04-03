d84f3033f0a91c2630188266dcff19ff6b7172f0
Linus Unnebäck <linus@folkdatorn.se>
2016-04-03 18:15:21 +0200

    coding style: standard

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
+var os = require('os')
+var env = process.env
 
-var os = require('os');
-var env = process.env;
+var implementation
 
-var implementation;
-
-switch(os.type()) {
+switch (os.type()) {
   case 'Darwin':
     implementation = function (id) {
-      return env.HOME + '/Library/Caches/' + id;
-    };
-    break;
+      return env.HOME + '/Library/Caches/' + id
+    }
+    break
   case 'Linux':
     implementation = function (id) {
-      return env.HOME + '/.cache/' + id;
-    };
-    break;
+      return env.HOME + '/.cache/' + id
+    }
+    break
   default:
-    throw new Error('Your OS is currently not supported by node-cachedir.');
+    throw new Error('Your OS is currently not supported by node-cachedir.')
 }
 
 module.exports = function (id) {
-  if(typeof id !== 'string') {
-    throw new TypeError('id is not a string');
+  if (typeof id !== 'string') {
+    throw new TypeError('id is not a string')
   }
-  if(id.length === 0) {
-    throw new Error('id cannot be empty');
+  if (id.length === 0) {
+    throw new Error('id cannot be empty')
   }
-  if(/[^0-9a-zA-Z-]/.test(id)) {
-    throw new Error('id cannot contain special characters');
+  if (/[^0-9a-zA-Z-]/.test(id)) {
+    throw new Error('id cannot contain special characters')
   }
 
-  return implementation(id);
-};
+  return implementation(id)
+}
